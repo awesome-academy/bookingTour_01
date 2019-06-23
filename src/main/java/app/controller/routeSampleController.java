030f66bc441589bc11ae639b0a9ac04b7b17e010
hienhtt-0013 <ho.thi.thu.hien@sun-asterisk.com>
2019-06-23 09:41:41 +0700

    Revert "add template admin"
    This reverts commit df235ece8a456762c81e03c9458cc741997c6709.

--- src/main/java/app/controller/routeSampleController.java
+++ src/main/java/app/controller/routeSampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class publicRouteSampleController {
+public class routeSampleController {
 
 	@RequestMapping(value = {"/","/index"}, method = RequestMethod.GET)
 	public String index() {
