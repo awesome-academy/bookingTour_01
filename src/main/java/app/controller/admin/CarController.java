367f1c88d13f935b8f3ad0f65a3236d8195d588c
minhtc-0015 <tran.cong.minh@sun-asterisk.com>
2019-06-25 23:01:44 +0700

    updateAndDeleteCar

--- src/main/java/app/controller/admin/CarController.java
+++ src/main/java/app/controller/admin/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import app.model.Car;
@@ -38,7 +39,23 @@ public class CarController {
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveCar(@ModelAttribute("car") Car car) {
+		System.err.println("-----------------------------------"+car.getId()+"-------------");
 		carService.saveOrUpdate(car);
 		return "redirect:/admin/cars/";
+	}
+	
+	@RequestMapping("/edit")
+	public ModelAndView editCustomerForm(@RequestParam int id) {
+	    ModelAndView mav = new ModelAndView("admin/cars/edit");
+	    Car car = carService.findById(id);
+	    mav.addObject("car", car);
+	 
+	    return mav;
+	}
+	
+	@RequestMapping("/delete")
+	public String deleteCustomerForm(@RequestParam int id) {
+	    carService.deleteCar(id);
+	    return "redirect:/admin/cars/";       
 	}
 }
